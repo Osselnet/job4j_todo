b75f26cc8b03150a94a12b18c1379a6e396db374
Oleg Sergienko <hornets@list.ru>
2022-05-17 14:56:58 +0300

    3. Лямбды и шаблон wrapper.

--- src/main/java/ru/job4j/todo/persistence/ItemDBStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemDBStore.java
@@ -2,6 +2,8 @@ package ru.job4j.todo.persistence;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -9,6 +11,7 @@ import ru.job4j.todo.model.Item;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 @Repository
 public class ItemDBStore implements Store<Item> {
@@ -22,91 +25,102 @@ public class ItemDBStore implements Store<Item> {
 
     @Override
     public Optional<Item> create(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        Optional<Item> result = Optional.ofNullable(session.get(Item.class, item.getId()));
-        session.getTransaction().commit();
-        session.close();
-        LOG.info("{}:{} заявка сохранена", item.getId(), item.getName());
-        return result;
+        return tx(
+                session -> {
+                    session.save(item);
+                    Optional<Item> result = Optional.ofNullable(session.get(Item.class, item.getId()));
+                    LOG.info("{}:{} заявка сохранена", item.getId(), item.getName());
+                    return result;
+                }
+        );
     }
 
     @Override
     public Optional<Item> findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Optional<Item> result = Optional.ofNullable(session.get(Item.class, id));
-        session.getTransaction().commit();
-        session.close();
-        LOG.info("Результат поиска по {}:{}", id, result.get().getName());
-        return result;
+        return tx(
+                session -> {
+                    Optional<Item> result = Optional.ofNullable(session.get(Item.class, id));
+                    LOG.info("Результат поиска по {}:{}", id, result.get().getName());
+                    return result;
+                }
+        );
     }
 
     @Override
     public boolean update(int id, Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        int result = session.createQuery("update Item set name = :nameItem, "
-                + "description = :descriptionItem, created = :createdItem, "
-                + "done = :doneItem where id = :idItem")
-                .setParameter("nameItem", item.getName())
-                .setParameter("descriptionItem", item.getDescription())
-                .setParameter("createdItem", item.getCreated())
-                .setParameter("doneItem", item.getDone())
-                .setParameter("idItem", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-        LOG.info("Результат обновления заявки {}", result > 0);
-        return result > 0;
+        return tx(
+                session -> {
+                    int result = session.createQuery("update Item set name=:name, description=:description, "
+                            + "created=:created, done=:done where id=:id")
+                            .setParameter("id", id)
+                            .setParameter("name", item.getName())
+                            .setParameter("description", item.getDescription())
+                            .setParameter("created", item.getCreated())
+                            .setParameter("done", item.getDone())
+                            .executeUpdate();
+                    LOG.info("Результат обновления заявки {}", result > 0);
+                    return result > 0;
+                }
+        );
     }
 
     public boolean delete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        int result = session.createQuery("delete from Item where id = :idItem")
-                .setParameter("idItem", id)
-                .executeUpdate();
-        session.getTransaction().commit();
-        session.close();
-        LOG.info("Результат удаления заявки {}", result > 0);
-        return result > 0;
+        return tx(
+                session -> {
+                    int result = session.createQuery("delete from Item where id = :id")
+                            .setParameter("id", id)
+                            .executeUpdate();
+                    LOG.info("Результат удаления заявки {}", result > 0);
+                    return result > 0;
+                }
+        );
     }
 
     @Override
     public List<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item")
-                .list();
-        session.getTransaction().commit();
-        session.close();
-        LOG.info("Найдено заявок {}", result.size());
-        return result;
+        return tx(
+                session -> {
+                    final Query query = session.createQuery("from Item");
+                    LOG.info("Найдено заявок {}", query.list().size());
+                    return query.list();
+                }
+        );
     }
 
     @Override
     public List<Item> findNew() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item  where done is null")
-                .list();
-        session.getTransaction().commit();
-        session.close();
-        LOG.info("Найдено новых заявок {}", result.size());
-        return result;
+        return tx(
+                session -> {
+                    final Query query = session.createQuery("from Item  where done is null");
+                    LOG.info("Найдено новых заявок {}", query.list().size());
+                    return query.list();
+                }
+        );
     }
 
     @Override
     public List<Item> findCompleted() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item  where done is not null")
-                .list();
-        session.getTransaction().commit();
-        session.close();
-        LOG.info("Найдено завершенных заявок {}", result.size());
-        return result;
+        return tx(
+                session -> {
+                    final Query query = session.createQuery("from Item  where done is not null");
+                    LOG.info("Найдено завершенных заявок {}", query.list().size());
+                    return query.list();
+                }
+        );
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
